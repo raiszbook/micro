3f812cbb9802d87e8ee35a41e2d8d936a95bd9d6
rcarbajh <raisz2012@gmail.com>
2024-01-23 18:40:48 -0500

    Primera parte : Creacion de analisis de microservicios y arquitectura. Uso SwaggerCode.

---
+++ src/test/java/com/proy/micro/MicroApplicationTests.java
@@ -0,0 +1,15 @@
+package com.proy.micro;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class MicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
